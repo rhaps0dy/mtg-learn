3aac2ccde794c0a5b7b4cfc8c9a59e8e5ed5ebd0
Adrià Garriga <adria@monkingme.com>
2015-08-13 16:43:32 +0200

    Revert "Using Task.sequence instead of TaskUtils.sequence"
    This reverts commit 3439c3c43e468be8f78a2d29287d262e77adc7b2.

--- src/elm/TaskUtils.elm
+++ src/elm/TaskUtils.elm
@@ -1,9 +1,22 @@
 module TaskUtils
-  ( formsToDrawTask
+  ( sequence
+  , formsToDrawTask
   ) where
 
 import Task
 import Graphics.Collage exposing (Form)
 import Native.TaskUtils
+
+{- sequence : List (Task.Task a ()) -> Task.Task a ()
+sequence taskList =
+  case taskList of
+    [] ->
+      Task.succeed ()
+    (x::xs) ->
+      List.foldr (\t t' -> Task.andThen t (\_ -> t')) x xs -}
+
+sequence : List (Task.Task a ()) -> Task.Task a ()
+sequence = Native.TaskUtils.combineTasks
+
 formsToDrawTask : String -> List Form -> Int -> Int -> a -> Task.Task String ()
 formsToDrawTask = Native.TaskUtils.formsToDrawTask
